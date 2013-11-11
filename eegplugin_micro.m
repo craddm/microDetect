54a200d217366945aa6a36126ee38f562ce14f3e
craddm <monklefish@gmail.com>
2013-11-11 17:49:43 +0100

    Updated menu

--- eegplugin_micro.m
+++ eegplugin_micro.m
@@ -1,6 +1,12 @@
 % eegplugin_micro
 % Detect and correct for microsaccades using eye channels without
-% concurrently recorded eye-tracking data
+% concurrently recorded eye-tracking data. The use of four eye-channel
+% montages is recommended; do not use bipolarized eye-channels (i.e.
+% VEOG/HEOG).
+%
+% Authors:
+% Matt Craddock, University of Leipzig; Jasna Martinovic, University of
+% Aberdeen
 
 function eegplugin_micro (fig, trystrs, catchstrs);
 
@@ -31,7 +37,6 @@ submenu = uimenu(toolsmenu,'label','Microsaccade correction','Separator','on');
 comrEOG = [trystrs.no_check '[EEG LASTCOM] = pop_rEOG(EEG);' catchstrs.store_and_hist];
 comDetect = [trystrs.no_check '[EEG LASTCOM] = pop_detect(EEG);' catchstrs.store_and_hist];
 comsaccICA = [trystrs.no_check '[EEG LASTCOM] = pop_saccICA(EEG);' catchstrs.new_and_hist];
-comICAtran = [trystrs.no_check '[EEG LASTCOM] = pop_ICAtran(EEG);' catchstrs.store_and_hist];
 comCorrs = [trystrs.no_check '[LASTCOM] = pop_eyeCorrs(EEG);' catchstrs.store_and_hist];
 comPlotMS = [trystrs.no_check '[LASTCOM] = pop_plotMicros(EEG);' catchstrs.store_and_hist];
 
@@ -41,7 +46,6 @@ comPlotMS = [trystrs.no_check '[LASTCOM] = pop_plotMicros(EEG);' catchstrs.store
 uimenu(submenu,'Label','Calculate rEOG','CallBack',comrEOG,'Separator','on');
 uimenu(submenu,'Label','Detect microsaccades','CallBack',comDetect);
 uimenu(submenu,'Label','Run ICA for detection','CallBack',comsaccICA);
-uimenu(submenu,'Label','Transfer ICA to full dataset','CallBack',comICAtran);
 uimenu(submenu,'Label','Plot ICA component correlations with eye channels','CallBack',comCorrs);
 uimenu(submenu,'Label','Plot detected microsaccades','CallBack',comPlotMS);
 
