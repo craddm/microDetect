3927efb753dd07c64663acc054a7851c3140f383
craddm <monklefish@gmail.com>
2013-11-18 15:44:09 +0100

    Added axis labels

--- plotMicros.m
+++ plotMicros.m
@@ -10,10 +10,14 @@ function plotMicros(EEG,args);
            bar(EEG.microS.binEdges,EEG.microS.sacRate,'histc');
            ymax = ceil(max(EEG.microS.sacRate)/5)*5;
            axis ([EEG.microS.binEdges(3) EEG.microS.binEdges(end-1) 0 ymax]);
+           ylabel('Detection rate (/s)');
+           xlabel('Time (ms)');
        case 0
            bar(EEG.microS.binEdges,EEG.microS.binnedSacs,'histc');
            ymax = ceil(max(EEG.microS.binnedSacs)/10)*10;
            axis ([EEG.microS.binEdges(3) EEG.microS.binEdges(end-1) 0 ymax]);
+           xlabel('Time (ms)');
+           ylabel('Number of microsaccades');
    end  
     
 end
